91e3d797b02683304f1411db61b839247f93f114
Glenn Hansen <hansenglenneirik@gmail.com>
2019-11-05 15:53:56 +0100

    added index and test.js

---
+++ HTML/javascript/test.js
@@ -0,0 +1,20 @@
+var counter = 0;
+while (counter < 5){
+    console.log(counter);
+    counter++;
+}
+console.log("Done with while");
+
+var jakke
+for(jakke=0; jakke<5; jakke++){
+    console.log(jakke);
+}
+
+console.log("Done with for");
+
+if (jakke=4){
+    console.log("Jakke is now 4");
+}
+else {
+    console.log("Still not 4");
+}
