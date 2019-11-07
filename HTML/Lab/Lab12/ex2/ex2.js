3a5030e6ee8f4b9262624a75275f9b90d4ad3187
Glenn Hansen <hansenglenneirik@gmail.com>
2019-11-07 19:56:48 +0100

    started ex2

--- HTML/Lab/Lab12/ex2/ex2.js
+++ HTML/Lab/Lab12/ex2/ex2.js
@@ -12,17 +12,7 @@ because the ex2.html is loading the menu-db.js file first.
 */ 
 
 //REMOVE CODE FROM HERE
-console.log("...Executing ex2.js file");
-console.log("........................");
-console.log("");
-console.log("Ice cream categories:");
-console.log(categories);
-console.log("All the ice creams grouped by category:")
-console.log(iceCreams);
-console.log("The first ice cream under the 'ice cream' category:")
-console.log("--Name: " + iceCreams["ice creams"][0].name);
-console.log("--Description: " + iceCreams["ice creams"][0].description);
-console.log("--Price: " + iceCreams["ice creams"][0].price + "NOK");
+
 //END REMOVE
 
 //Your implementation goes here
