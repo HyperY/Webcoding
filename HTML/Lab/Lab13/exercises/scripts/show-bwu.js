98ef7bf7d931882df0d2ff042c8ee912aae5e055
Glenn Hansen <hansenglenneirik@gmail.com>
2019-11-14 15:30:11 +0100

    Finished ex2

--- HTML/Lab/Lab13/exercises/scripts/show-bwu.js
+++ HTML/Lab/Lab13/exercises/scripts/show-bwu.js
@@ -4,9 +4,20 @@ it is a good practice to encapsulate the code inside an  anonymous function that
 In new JS versions, this is solved using modules.
 Anyway, don't worry about that, you will learn that stuff in other courses.
 */
-(function () {
+(function showCourses() {
     //your code goes here...
     //a) create loop for going through courses["bwu"].
     //   With courses["bwu"].length you will know the number of elements of this list.
     //b) don't forget to add the elements created to the body (append...)
+    var bwuCourseList = courses["bwu"];
+    var unorderedList = document.createElement("ul");
+    var coursePos;
+    for (coursePos = 0; coursePos < bwuCourseList.length; coursePos++) {
+        var listItem = document.createElement("li");
+        var currentCourse = bwuCourseList[coursePos];
+        listItem.innerHTML = `
+            Course[${coursePos}] => ${currentCourse.code} - ${currentCourse.name}: ${currentCourse.description} . LEVEL ${currentCourse.level}`;
+            unorderedList.appendChild(listItem);
+    }
+    document.body.appendChild(unorderedList);
 })();
--- HTML/Lab/Lab13/exercises/scripts/show-programmes.js
+++ HTML/Lab/Lab13/exercises/scripts/show-programmes.js
@@ -4,7 +4,7 @@ it is a good practice to encapsulate the code inside an anonymous function that 
 In new JS versions, this is solved using modules.
 Anyway, don't worry about that, you will learn that stuff in other courses.
 */
-//console.log(programmes);
+console.log(programmes);
 (function showProgrammes() {
     //your code goes here...
     // a) loop for "programmes". Use programmes.length to know the number of elements of this list.
