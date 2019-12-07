a46523b74a8af7b70631c635ea45ee7aafb50b03
Glenn Hansen <hansenglenneirik@gmail.com>
2019-12-07 14:53:27 +0100

    fixed bugs

--- HTML/oblig3_4/coursework3-sources/programmes-page/functions.js
+++ HTML/oblig3_4/coursework3-sources/programmes-page/functions.js
@@ -1,8 +1,27 @@
 // console.log(courses["bwu"]);
+/*
+function all() {
+    var helpers = HELPERS();
+    var idContainer = "programmes-container";
 
+    var i, j;
+    for(i = 0; i < programmes.length; i++) {
+        var h2 = document.createElement("h2");
+        h2.innerHTML = programmes[i];
+        document.getElementById(idContainer).appendChild(h2);
+
+        var currentCourses = courses[programmes[i]];
+        for(j = 0; j < currentCourses.length; j++) {
+            var menuItem = helpers.getHTMLCourseFromCodeNameDescriptionLevel(currentCourses[j].code, currentCourses[j].name, currentCourses[j].description, currentCourses[j].level);
+            document.getElementById(idContainer).appendChild(menuItem);
+        }
+    }
+}
+*/
 function bwu() {
     var helpers = HELPERS();
     var bwuCourses = courses["bwu"];
+    document.getElementById("course").innerHTML = "";
     
     for(var i = 0; i < bwuCourses.length; i++) {
         var courseItem = helpers.getHTMLCourseFromCodeNameDescriptionLevel(bwuCourses[i].code, bwuCourses[i].name, bwuCourses[i].description, bwuCourses[i].level);
@@ -13,6 +32,7 @@ function bwu() {
 function bixd() {
     var helpers = HELPERS();
     var bixdCourses = courses["bixd"];
+    document.getElementById("course").innerHTML = "";
 
     for(var i = 0; i < bixdCourses.length; i++) {
         var courseItem = helpers.getHTMLCourseFromCodeNameDescriptionLevel(bixdCourses[i].code, bixdCourses[i].name, bixdCourses[i].description, bixdCourses[i].level);
@@ -23,6 +43,7 @@ function bixd() {
 function bmed() {
     var helpers = HELPERS();
     var bmedCourses = courses["bmed"];
+    document.getElementById("course").innerHTML = "";
 
     for(var i = 0; i < bmedCourses.length; i++) {
         var courseItem = helpers.getHTMLCourseFromCodeNameDescriptionLevel(bmedCourses[i].code, bmedCourses[i].name, bmedCourses[i].description, bmedCourses[i].level);
@@ -33,6 +54,7 @@ function bmed() {
 function arweb() {
     var helpers = HELPERS();
     var arwebCourses = courses["arweb"];
+    document.getElementById("course").innerHTML = "";
 
     for(var i = 0; i < arwebCourses.length; i++) {
         var courseItem = helpers.getHTMLCourseFromCodeNameDescriptionLevel(arwebCourses[i].code, arwebCourses[i].name, arwebCourses[i].description, arwebCourses[i].level);
@@ -43,6 +65,7 @@ function arweb() {
 function mixd() {
     var helpers = HELPERS();
     var mixdCourses = courses["mixd"];
+    document.getElementById("course").innerHTML = "";
 
     for(var i = 0; i < mixdCourses.length; i++) {
         var courseItem = helpers.getHTMLCourseFromCodeNameDescriptionLevel(mixdCourses[i].code, mixdCourses[i].name, mixdCourses[i].description, mixdCourses[i].level);
